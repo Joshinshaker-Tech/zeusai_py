077af2ef0c5623697b22eb313ba00b13a06f0ad9
Josh Levin <floydaroo@gmail.com>
2021-08-25 19:18:54 -0400

    Added the followup() function

--- src/zeusai_py/plugin.py
+++ src/zeusai_py/plugin.py
@@ -52,7 +52,7 @@ def variable(variables: list) -> dict:
     :param variables: List of variables to get the values of
     :type variables: List
 
-    :raise: Everything in _get_response(), AND:
+    :raise: See _get_response().
     :return: Dictionary, containing {key: value} pairs: {variable_name: value}
     """
     _send_request("variable", variables)
@@ -60,12 +60,16 @@ def variable(variables: list) -> dict:
     return response["params"]
 
 
-def followup() -> None:
-    """
-    :raise:
+def followup(question: str) -> None:
+    """ Ask the user a followup question, and get their response.
+
+    :param question: The question to ask the user.
+    :type question: String
+
+    :raise: See _get_response().
     :return:
     """
-    pass
+    _send_request("followup", question)
 
 
 def simulate_client() -> client.Client:
@@ -75,7 +79,7 @@ def simulate_client() -> client.Client:
     NOTE: THIS IS IRREVERSIBLE! ONCE THE PLUGIN IS USING THE CLIENT API, IT CANNOT
     GO BACK TO THE PLUGIN API.
 
-    :raise:
+    :raise: See _get_response().
     :return: Instance of Client
     """
     _send_request("simulate client", "")

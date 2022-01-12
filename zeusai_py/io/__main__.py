b0d385b290f06caa8d6d80eecb7bf51149dc60a1
Josh Levin <floydaroo@gmail.com>
2022-01-12 13:01:46 -0500

    (#10) Added defaults

--- zeusai_py/io/__main__.py
+++ zeusai_py/io/__main__.py
@@ -1,9 +1,11 @@
 """
 When the IO module is run directly, it launches a basic interactive CLI client
 .
-The __main__ module also works as an example of a basic client for other developers
+The __main__ module also works as a good example of a simple client for other developers
+to begin their plugins from.
 """
 from zeusai_py import io
+from zeusai_py.io import client
 import getpass
 
 
@@ -13,17 +15,29 @@ def output(ai_out: str):
 
 
 def main():
-    host = input("ZeusAI Server Host: ")
-    port = int(input("ZeusAI Server Port: "))
-    client = io.Client(host, port)
+    host = input("ZeusAI Server Host (localhost):")
+    port = int(input("ZeusAI Server Port (9387):"))
+
+    # Default host and port
+    if host is None:
+        host = "127.0.0.1"
+    if port is None:
+        port = "9387"
+
+    # Actual Client Code - Very simple to use.
+    client_ = client.Client(host, port)
+
+    # Authenticate
     username = input("Username: ")
     password = getpass.getpass()
-    client.authenticate(username, password)
-    client.set_output_func(output)
-    client.recv_thread.start()
+    client_.authenticate(username, password)
+
+    # Set the o
+    client_.set_output_func(output)
+    client_.recv_thread.start()
     while True:
         user_input = input("Input: ")
-        client.input(user_input)
+        client_.input(user_input)
 
 
 if __name__ == "__main__":
